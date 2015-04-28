a1d55828e7eae1b896f36b97158612925e4e80d0
bmac1993 <bmac1993@gmail.com>
2015-04-28 12:14:47 -0400

    Added plugin

---
+++ Plugin/Plugin/Plugin/start.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugin
+{
+    public class start
+    {
+        IIdGenerator Plugin { get; set; }
+
+        public start()
+        {
+            Plugin = new IdGenerator();
+
+            Console.WriteLine();
+        }
+    }
+}
